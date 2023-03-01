2e7c7eaebca70a9ff2d95573ba2db2b3ae6a230b
Artem Ivanov <60775519+lMercyl@users.noreply.github.com>
2023-03-01 15:08:41 +0300

    Add files via upload

--- src/model3d.php
+++ src/model3d.php
@@ -1,33 +1,25 @@
 <?php
-$db = [
-  "boots" =>
-  [
-    "name" => "nike",
-    "price" => "16000"
-  ],
-  "worker" =>
-  [
-    "name" => "artem",
-    "pay" => "30000"
-  ]
-]
-?>
+$my_array = array(
+  "Обувь" => array(
+    "nike" => array("10000руб", "нет в наличии"),
+    "col2" => array("value3", "value4")
+  ),
+  "row2" => array(
+    "col1" => array("value5", "value6"),
+    "col2" => array("value7", "value8")
+  )
+);
 
-<table border="1" align="center">
-  <tr>
-    <td colspan="2" align="center">shop</td>
-    <td colspan="2" align="center">personal</td>
-  </tr>
-  <tr>
-    <?php foreach ($db as $item) : ?>
-      <td colspan="2" align="center"><?php echo key($item) ?></td>
-    <?php endforeach ?>
-  </tr>
-  <tr>
-    <?php foreach ($db as $item) : ?>
-      <?php foreach ($item as $value) : ?>
-        <td align="center"><?php echo $value ?></td>
-      <?php endforeach ?>
-    <?php endforeach ?>
-  </tr>
-</table>
+echo "<table border='2'>";
+foreach ($my_array as $row => $columns) {
+  echo "<tr>";
+  foreach ($columns as $column => $values) {
+    echo "<td>";
+    foreach ($values as $value) {
+      echo "$value ";
+    }
+    echo "</td>";
+  }
+  echo "</tr>";
+}
+echo "</table>";
