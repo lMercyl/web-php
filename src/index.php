feb2db194258a60533231ff4130f42b4ae2ca4d7
Artem Ivanov <60775519+lMercyl@users.noreply.github.com>
2023-03-02 22:17:01 +0300

    Add files via upload

--- src/index.php
+++ src/index.php
@@ -1,3 +1,28 @@
+<?php
+$randSelect = rand(1, 3);
+
+$shop = [
+  ["name" => "Big Ocean shop", "address" => "Saint Petersburg, Big Ocean 68", "time" => "08:00 - 13:00", "phone" => "+79214456758"],
+  ["name" => "Gastello shop", "address" => "Saint Petersburg, Gastello", "time" => "07:30 - 18:00", "phone" => "+79214456758"],
+  ["name" => "Lenca shop", "address" => "Saint Petersburg, Lencsovet", "time" => "07:30 - 21:00", "phone" => "+79214456758"]
+];
+
+$my_array = array(
+  "Обувь" => array(
+    "nike" => array("10000руб", "нет в наличии"),
+  ),
+  "Штаны" => array(
+    "nike" => array("10000руб", "нет в наличии"),
+  ),
+  "Куртка" => array(
+    "nike" => array("10000руб", "нет в наличии"),
+  ),
+  "Кепка" => array(
+    "nike" => array("10000руб", "нет в наличии"),
+  ),
+);
+?>
+
 <!DOCTYPE html>
 <html lang="ru">
 
@@ -50,14 +75,70 @@
         </tr>
       </table>
     </div>
-    <div style="display: none; margin-top: 20px" class="list-container">
-      <?php include("list.php") ?>
+    <div style="margin-top: 20px" class="list-container">
+      <p class="text" align="center">
+        <?php
+        switch ($randSelect) {
+          case 1:
+            echo "1. Встал утром <br />Позавтракал <br />Собрался в вуз<br />Приехал домой и лег спать";
+            break;
+          case 2:
+            echo "2. Привет как дела<br />Как погода ха-ха <br />Вышел на улицу<br />Поджарил курицу";
+            break;
+          case 3:
+            echo "3. Теорема пифагоsа<br />Сумма квадратов катетов<br />Равна квадработу гипотенузы<br />Доказано";
+            break;
+        }
+        ?>
+      </p>
     </div>
-    <div style="display: none; margin-top: 20px" class="table-model-container">
-      <?php include("model.php") ?>
+    <div style="margin-top: 20px" class="table-model-container">
+      <table border="1" align="center">
+        <tr>
+          <td align="center">Название</td>
+          <td align="center">Адресс</td>
+          <td align="center">Время работы</td>
+          <td align="center">Телефон</td>
+        </tr>
+        <?php foreach ($shop as $item) : ?>
+          <tr>
+            <?php foreach ($item as $key => $value) : ?>
+              <td align="center">
+                <?php echo $value ?>
+              </td>
+            <?php endforeach ?>
+          </tr>
+        <?php endforeach ?>
+      </table>
     </div>
-    <div style="display: none; margin-top: 20px" class="table-model3d-container">
-      <?php include("model3d.php") ?>
+    <div style="margin-top: 20px" class="table-model3d-container">
+      <?php
+      echo "<table align='center' border='2'>";
+      echo "<tr>";
+      echo "<td align='center' colspan='4'>";
+      echo "Низ";
+      echo "</td>";
+      echo "<td align='center' colspan='4'>";
+      echo "Верх";
+      echo "</td>";
+      echo "</tr>";
+      echo "<tr>";
+      foreach ($my_array as $row => $value) {
+        echo "<td align='center' colspan='2'>";
+        echo $row;
+        echo "</td>";
+      }
+      echo "</tr>";
+      foreach ($my_array as $row => $columns) {
+        foreach ($columns as $column => $values) {
+          foreach ($values as $value) {
+            echo "<td align='center'>";
+            echo "$value";
+            echo "</td>";
+          }
+        }
+      }
+      echo "</table>"; ?>
     </div>
   </div>
 
